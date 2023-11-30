d835b0d741a2a8bdca383788d553d854acde9781
scotcy <kimjh470@naver.com>
2023-11-30 16:34:29 +0900

    똥 생성&파괴 구현(수정)
    Todo : Controller에서 난이도 높이기 구현

--- Assets/Scripts/Pooh/PoohCollision.cs
+++ Assets/Scripts/Pooh/PoohCollision.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PoohCollision : MonoBehaviour
 {
     //여기에는 충돌처리를 구현해준다.
+    PoohSpawn _poohSpawn;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Pooh"))
         {
             Destroy(collision.gameObject);
+            _poohSpawn.spawnRate ++;
         }
         else if (collision.gameObject.CompareTag("Player"))
         {
             // 게임오버
+            //게임 오버 UI창 불러오기
+            //시간 멈추기
         }
         // 나중에 다른 태그가 붙은 똥에 대한 충돌을 구현하고싶으면 위에랑 똑같이 tag만 수정해서 추가해주면 된다.
     }
--- Assets/Scripts/Pooh/PoohController.cs
+++ Assets/Scripts/Pooh/PoohController.cs
@@ -7,7 +7,9 @@ public class PoohController : MonoBehaviour
     // 여기는 똥을 몇개나 피했는지 카운트 해주고 그 수 만큼 똥 생성을 많이 해준다.
     // 그러면 Collision에서 카운트 받아오고, Spawn률을 증가시켜주기.
 
-    // Start is called before the first frame update
+
+    static int count;
+    
     void Start()
     {
         

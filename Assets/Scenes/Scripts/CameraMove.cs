3d6020971407e8160d21bd8929283c354b2949f3
doia7002 <doia7002@naver.com>
2023-12-01 21:32:26 +0900

    Error delete

--- Assets/Scenes/Scripts/CameraMove.cs
+++ Assets/Scenes/Scripts/CameraMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 Camera;
+    float Y;
+
+
+    private void Awake()
     {
-        
+        Camera = new Vector2(0, Y);
+        Y = -10f;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if (Y < 0)
+        {
+            Y += Time.deltaTime;
+            transform.Translate(Vector2.up * 1f * Time.deltaTime);
+        }
+        else
+        {
+
+        }
+
+
     }
 }

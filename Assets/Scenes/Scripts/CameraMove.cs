e87027e4356b98c7fd98f7fe9d9b711432ffcf6d
doia7002 <doia7002@naver.com>
2023-12-06 17:46:33 +0900

    introScene수정
    카메라 위치, 타이틀 버튼 비활성화

--- Assets/Scenes/Scripts/CameraMove.cs
+++ Assets/Scenes/Scripts/CameraMove.cs
@@ -1,33 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraMove : MonoBehaviour
 {
-    private Vector2 Camera;
+    private Vector2 CameraPosition;
     float Y;
+    float LastCameraY = 0f;
+    private bool state;
+    public GameObject Target;
+    public GameObject button;
+    private GameObject Camera;
+    
 
-
+   
+    private void Start()
+    {
+        state = false;
+    }
     private void Awake()
     {
-        Camera = new Vector2(0, Y);
-        Y = -10f;
+        CameraPosition = new Vector2(0, Y);
+        Y = -7.7f;
+        Camera=GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     private void Update()
     {
 
-        if (Y < 0)
+        if (transform.position.y < 0)
         {
-            Y += Time.deltaTime;
-            transform.Translate(Vector2.up * 1f * Time.deltaTime);
+            
+            transform.Translate(Vector3.up * 1f * Time.deltaTime);
+            if (Input.anyKeyDown)
+            {
+                Debug.Log("ÀÔ·ÂµÊ");
+                
+                transform.position = new Vector3(0, 0, -10);
+            }
+            
+            
         }
-        else
+        else  
         {
-
+            
+            Target.SetActive(true);
+            button.SetActive(true);
+            state = true;
         }
-
+        
+        
+        
 
     }
+
+    
+        
+        
+    
 }
